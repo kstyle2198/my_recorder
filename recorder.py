9cda71c2858a88d3e721f0cccfdba2c043f01734
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-11-21 11:48:42 +0900

    3rd - try, excpet

--- recorder.py
+++ recorder.py
@@ -5,28 +5,31 @@ import pandas as pd
 list1 = []
 
 while True:
-    print("ready to press P")
+    print("Please press P")
     if keyboard.read_key() == "p":
-        print("pressed P")
+        print("You have pressed P")
         r = sr.Recognizer()
-        print("ready to record")
+        print("Ready to record")
         with sr.Microphone() as source1:
-            audio1 = r.listen(source1)
-            print("소리를 글자로 변환중...")
-            text1 = r.recognize_google(audio1, language="ko-KR")
+            try:
+                audio1 = r.listen(source1)
+                print("소리를 글자로 변환중...")
+                text1 = r.recognize_google(audio1, language="ko-KR")
 
-            print("-"*20)
-            print(text1)
-            print("="*20)
+                print("-"*20)
+                print(text1)
+                print("="*20)
 
-            list1.append(text1)
-            df = pd.DataFrame(list1)
-            df.to_csv(
-                "C:/my_develop2/meeting_record/records/meetings.csv",
-                header=False,
-                index=False,
-                encoding="euc-kr'",
-            )
-            print("부분 녹음 종료")
+                list1.append(text1)
+                df = pd.DataFrame(list1)
+                df.to_csv(
+                    "C:/my_develop2/meeting_record/records/meetings.csv",
+                    header=False,
+                    index=False,
+                    encoding="euc-kr'",
+                )
+                print("부분 녹음 종료")
+            except:
+                print("뭔소리고????")
     else:
         pass
