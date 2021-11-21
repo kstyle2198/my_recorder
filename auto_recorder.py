db544fc173534df7b9320c4e19e3ec8489a2dfc3
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-11-21 14:34:43 +0900

    5th - correct typos

--- auto_recorder.py
+++ auto_recorder.py
@@ -2,27 +2,26 @@ import speech_recognition as sr
 import keyboard
 import pandas as pd
 
-# ko-KR    ja-JP    zh   en-US
+# 사용할 언어와 저장할 파일명을 정해주세요.     한국어 ko-KR    일본어 ja-JP    중국어(북경어) zh   영어   en-US
 
 사용언어 = "ko-KR"
-저장파일명 = "meeting_auto1.csv"
-
+저장파일명 = "meeting_2021124.csv"
 list1 = []
 
 while True:
     r = sr.Recognizer()
     print("Ready to record")
-    with sr.Microphone() as source1:
+    with sr.Microphone() as source:
         try:
-            audio1 = r.listen(source1)
+            audio = r.listen(source)
             print("소리를 글자로 변환중...")
-            text1 = r.recognize_google(audio1, language=사용언어)
+            text = r.recognize_google(audio, language=사용언어)
 
             print("-"*20)
-            print(text1)
+            print(text)
             print("="*20)
 
-            list1.append(text1)
+            list1.append(text)
             df = pd.DataFrame(list1)
             df.to_csv(
                 "C:/my_develop2/meeting_record/records/{}".format(저장파일명),
